2b9a6f616eb021cb3351c568edc593f271dbba83
Lisa Roach <lisroach@cisco.com>
2015-08-06 08:18:42 -0700

    Commented

--- int_brief.py
+++ int_brief.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python
+
+############################################################################################################
+#		Name: int_brief.py
+#		Author: Lisa Roach
+#		Date: 8/5/2015
+#		Description: The point of this script is to output the ip interface brief command for al the 9k's I have access to
+#		Notes: The loops in main() should be adjusted according to the switches you are accessing. The main use of this script is the 
+#		show_ip_int_brief function
+#
+###############################################################################################################
+
 import xmltodict
 import json
 import sys
@@ -44,29 +55,35 @@ def show_ip_int_brief(sw, identity):
 
 def main():
 
-	#need to pull the switch data
+	#The for loops are for looping through the multiple ip addresses of my switches
+	#The addresses are not entirely in order, so multiple loops were required
 
-	for i in range(133, 139):
-		ip_address = '172.31.217.' + str(i)
-		switch = Device(ip=ip_address, username='admin', password='cisco123')
+
+	#change the ranges and ip_appresses to match your switches
+	#Change the username and password to match your switches as well.
+
+	for i in range(111, 111):
+		ip_address = '111.111.111.' + str(i)
+		switch = Device(ip=ip_address, username='username', password='password')
 		#open the switch connection
 		switch.open()
 		#call the  function
 		show_ip_int_brief(switch, ip_address)
 
 
-	for i in range(142, 145):
-		ip_address = '172.31.217.' + str(i)
-		switch = Device(ip=ip_address, username='admin', password='cisco123')
+	for i in range(111, 111):
+		ip_address = '###.###.###' + str(i)
+		switch = Device(ip=ip_address, username='username', password='password')
 		#open the switch connection
 		switch.open()
 
 		#call the  function
 		show_ip_int_brief(switch, ip_address)
 
-		#changed from 149
-	ip_address='172.31.217.149'
-	switch = Device(ip=ip_address, username='admin', password='cisco123')
+	#This is an example of a sinle ip address not in order. 
+	
+	ip_address='111.111.111.111'
+	switch = Device(ip=ip_address, username='username', password='password')
 	switch.open()
 
 	#call the  function
