3ec79fe35cfb3b707190fcd4b35ec20651e7466c
Lisa Roach <lisroach@cisco.com>
2015-10-01 11:49:35 -0700

    output working

--- Neighbors/l3_scrape.py
+++ Neighbors/l3_scrape.py
@@ -1,33 +1,31 @@
+__author__ = "Darius Carrier"
+#Credit: https://thepacketgeek.com/monitor-your-bgp-advertised-routes-with-paramiko/
+
 import paramiko
 import re
 from time import sleep
 
-def bgpcreation(username,password,f):
+def bgpcreation(crawler, f):
 	### Please Enter the First Switch MGMT IP ###
 	#switchIP= raw_input("Please Enter The First Switch MGMT IP: ")
 	#Create the shell object
-	f.write('[BGP]')
-	f.write('\n')
-	#Open the file for IP List
-	filename= "mgmtIPs.txt"
-	with open(filename, 'r') as fp:
-		for address in fp:
-			address = address.rstrip()
-			address = address.replace("'", "")
-			if address:
-				createtheGoods(address,username,password,f)
+	#print "Made it to BGP"
+	createtheGoods(crawler,f)
 
-def createtheGoods(switchIP,username,password,f):
+def createtheGoods(crawler, f):
 	ssh= paramiko.SSHClient()
 	ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	try:
-		ssh.connect(switchIP, 22, username, password, look_for_keys=False)
+		ssh.connect(crawler.current_address, 22, crawler.username, crawler.password, 
+			look_for_keys=False)
 	except (paramiko.transport.socket.error,
         paramiko.transport.SSHException,
         paramiko.transport.socket.timeout,
-        paramiko.util.log_to_file("filename.log"),
-        paramiko.auth_handler.AuthenticationException):
-		print 'Error connecting to SSH on %s' % switchIP
+        paramiko.auth_handler.AuthenticationException,
+        paramiko.util.log_to_file("filename.log")):
+		print 'Error connecting to SSH on %s' % crawler.current_address
+		return None
+	#print "I connected"
 	shell = ssh.invoke_shell()
 	shell.settimeout(3)
 	bgpoutput = getL3(shell, '\nshow ip bgp summ | b Neigh\n')
@@ -46,13 +44,16 @@ def createtheGoods(switchIP,username,password,f):
 	    else:
 	        ip_regex = r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
 	        peer_ips.extend(re.findall(ip_regex, line))
-	print peer_ips
+	#print peer_ips
+	#if peer_ips:
+		#f.write('[BGP]')
+		#f.write('\n')
 	for n in peer_ips:
 		for line in bgpoutput.split('\n'):
 			test= line.split()
 			if test[0] == n:
 				writetoFile(f,test)
-	
+	f.write('\n')	
 
 def getL3(shell, command):
 	shell.send(command.strip() + '\n')
